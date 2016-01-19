f144123d4433d967a09e5b0b5733f12f9f2ecf16
Griffin Hammer <grifhammer@gmail.com>
2016-01-19 18:18:45 -0500

    Changed to hard coding robs email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,35 +1,57 @@
+// <?php
+
+
+// // Check for empty fields
+// if(empty($_POST['name'])  		||
+//    empty($_POST['email']) 		||
+//    empty($_POST['phone']) 		||
+//    empty($_POST['message'])	||
+//    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+//    {
+// 	echo "No arguments Provided!";
+// 	return false;
+//    }
+	
+// $name = $_POST['name'];
+// $email_address = $_POST['email'];
+// $phone = $_POST['phone'];
+// $message = $_POST['message'];
+
+//     require_once('/usr/share/php/libphp-phpmailer/class.phpmailer.php');
+//     $mail = new PHPMailer(); // defaults to using php "mail()"
+//     $mail->AddReplyTo($email,$name);
+//     $mail->SetFrom($email, 'From '.$name);
+//     $address = "grifhammer@gmail.com";
+//     $mail->AddAddress($address, "Griffin Hammer");
+//     $mail->Subject    = "Contact email from Portfolio";
+//     $mail->MsgHTML($message);
+//     if(!$mail->Send()) {
+//        echo "Mailer Error: " . $mail->ErrorInfo;
+//        return false
+//     } else {
+//     echo "Message sent!";
+//     return true;
+//     }
+// ?>
+
 <?php
 
-
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+ $name = 'Robert Bunch';
+ $email = 'rdbunch@gmail.com';
+ $tel = '712-229';
+ $message = 'here is a message';
 
     require_once('/usr/share/php/libphp-phpmailer/class.phpmailer.php');
     $mail = new PHPMailer(); // defaults to using php "mail()"
     $mail->AddReplyTo($email,$name);
     $mail->SetFrom($email, 'From '.$name);
-    $address = "grifhammer@gmail.com";
+    $address = "rdbunch@gmail.com";
     $mail->AddAddress($address, "Robert D Bunch");
     $mail->Subject    = "Contact email from Portfolio";
     $mail->MsgHTML($message);
     if(!$mail->Send()) {
-       echo "Mailer Error: " . $mail->ErrorInfo;
-       return false
+    echo "Mailer Error: " . $mail->ErrorInfo;
     } else {
     echo "Message sent!";
-    return true;
     }
 ?>
